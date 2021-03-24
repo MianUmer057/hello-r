120efd2d98fb09cad45580a8f1da2521815134e3
Umer Farooq <umerfarooqjly10@gmail.com>
2021-03-24 07:04:21 +0500

    A few lines added

--- testing.r
+++ testing.r
@@ -1,4 +1,11 @@
 #This is from Github
 print("Hello, from Github")
 
+name = "Umer"
+version = "RStudio - in writing"
+
+
+print(name)
+print("These lines has been written in RStudio")
+print(version)
 
