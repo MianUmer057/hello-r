7e8460361dfb6ba6058bcc003d755a9436010ff2
Umer Farooq <umerfarooqjly10@gmail.com>
2021-03-24 07:18:37 +0500

    brief introduction about me

---
+++ introduction.R
@@ -0,0 +1,12 @@
+# brief introduction of me
+
+name = "umer farooq"
+job = "School Teacher"
+job_loc = "Kallah Kalan"
+description = "I'm a school teacher appointed in 2018 as a SSE computer science
+               Teacher an after that I am still working there...!"
+print(name)
+print(job)
+print(job_loc)
+print(description)
+
